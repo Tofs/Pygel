68b9f545c9db787e74cf3b329f96118ff702c7de
U-GLOBAL\AEISCF <AEISCF@RD0041699.global.scd.scania.com>
2015-12-23 22:32:57 +0100

    broken dont use, moving out files

--- main.py
+++ main.py
@@ -6,36 +6,6 @@ import OpenGLUtils as Utils
 import glfw
 from OpenGL.GL import *
 
-VERTEX_SHADER = """
-#version 330
-
-layout (location=0) in vec4 position;
-layout (location=1) in vec4 color;
-
-smooth out vec4 theColor;
-
-void main()
-{
-    gl_Position = position;
-    theColor = color;
-}
-"""
-
-FRAGMENT_SHADER = """
-#version 330
-
-smooth in vec4 theColor;
-out vec4 outputColor;
-
-void main()
-{
-    outputColor = theColor;
-}
-"""
-
-
-
-
 def main():
     Utils.init()
     window = Utils.createWindow()
