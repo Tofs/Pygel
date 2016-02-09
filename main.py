fd78ee39f8a78408be9e2765ab11732741a3ca29
Anton Einarson <anton.einarson@gmail.com>
2016-02-09 22:05:19 +0100

    i think i see the problem now

--- main.py
+++ main.py
@@ -16,11 +16,26 @@ def KillProgram():
     glfw.set_window_should_close(window, GL_TRUE)
 
 def Culling():
+    return
     glEnable(GL_CULL_FACE)
     glCullFace(GL_BACK)
     glFrontFace(GL_CCW) # GL_CW for clockwise and GL_CCW for counter clockwise
 
-def init():
+
+
+def createVAO(vertexPos, vertexColor, faces,size = 3, singleBuffer = False):
+    if singleBuffer:
+        return  singleVAO(vertexPos, vertexColor, faces, size)
+    else:
+        return multiVAO(vertexPos, vertexColor, faces, size)
+
+def multiVAO(vertexPos, vertexColor, faces, size):
+
+    return VAO, vertexCount, indexBuffer
+
+def main():
+    global window
+    initLogger()
     version_string = glfw.get_version_string();
     cwd = os.getcwd()
     print ("Starting GLFW")
@@ -38,13 +53,24 @@ def init():
     glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
 
 
-def createVAO(vertexPos, vertexColor, faces,size = 3, singleBuffer = False):
-    if singleBuffer:
-        return  singleVAO(vertexPos, vertexColor, faces, size)
-    else:
-        return multiVAO(vertexPos, vertexColor, faces, size)
-        
-def multiVAO(vertexPos, vertexColor, faces, size):
+
+    window = Utils.createWindow(inputHandler.keyboardListner)
+    # triangle position and color
+
+    vertexPos, vertexColor, faces = tmf.load("testData.tmf")
+
+
+    glEnable(GL_DEPTH_TEST)
+    print vertexPos
+    print faces
+
+    #define input
+    inputHandler.addEvent(KillProgram, "Esc")
+    inputHandler.addEvent(Culling, "c")
+
+    # VAO, VertexSize, indexs = createVAO(vertexPos, vertexColor, triangle, 4)
+    size = 4
+
     numPos = numpy.array(vertexPos, dtype=numpy.float32)
     numColor = numpy.array(vertexColor, dtype=numpy.float32)
     numIndex = numpy.array(faces, dtype=numpy.uint32)
@@ -55,8 +81,7 @@ def multiVAO(vertexPos, vertexColor, faces, size):
     #gen buffers
     VAO = glGenVertexArrays(1)
     pos = glGenBuffers(1)
-    color = glGenBuffers(1)
-    indexBuffer = glGenBuffers(1)
+    index = glGenBuffers(1)
 
     glBindVertexArray(VAO)
 
@@ -64,49 +89,19 @@ def multiVAO(vertexPos, vertexColor, faces, size):
     glBindBuffer(GL_ARRAY_BUFFER, pos)
     glBufferData(GL_ARRAY_BUFFER, numPos.nbytes, numPos, GL_STATIC_DRAW)
 
-    # create color VBO
-    glBindBuffer(GL_ARRAY_BUFFER, color)
-    glBufferData(GL_ARRAY_BUFFER, numColor.nbytes, numColor, GL_STATIC_DRAW)
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index)
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, numIndex.nbytes, numIndex, GL_STATIC_DRAW)
 
-    # enable array and set up data
-    glEnableVertexAttribArray(0)
-    glEnableVertexAttribArray(1)
     glBindBuffer(GL_ARRAY_BUFFER, pos)
     glVertexAttribPointer(0, size, GL_FLOAT, GL_FALSE, 0, None)
-    # the last parameter is a pointer to tell the offset between
-    # python donot have pointer, have to using ctypes
-    glBindBuffer(GL_ARRAY_BUFFER, color)
-    glVertexAttribPointer(1, size, GL_FLOAT, GL_FALSE, 0, None)
 
 
-    return VAO, vertexCount, indexBuffer
 
-def main():
-    global window
-    initLogger()
-    init()
-    window = Utils.createWindow(inputHandler.keyboardListner)
-    # triangle position and color
 
-    vertexPos, vertexColor, triangle = tmf.load("testData.tmf")
-
-    #define input
-    inputHandler.addEvent(KillProgram, "Esc")
-    inputHandler.addEvent(Culling, "c")
-
-    print vertexPos
-    VAO, VertexSize, indexs = createVAO(vertexPos, vertexColor, triangle, 4)
     shaderProgram = ShaderM.compileShaderFromFile("shaders/basic.vert", "shaders/basic.frag")
 
-    objectToRender = { "Indexs" : indexs, "VAO" : VAO, "VertexCount" : VertexSize, "ShaderProgram" : shaderProgram}
+    mainLoopObject = { "Indexs" : index, "VAO" : VAO, "VertexCount" : vertexCount, "ShaderProgram" : shaderProgram}
 
-    mainLoop(objectToRender,window)
-    glfw.terminate()
-
-def mainLoop(mainLoopObject, window):
     VAO = mainLoopObject["VAO"]
     i = mainLoopObject["Indexs"]
     VertexCount = mainLoopObject["VertexCount"]
@@ -114,24 +109,29 @@ def mainLoop(mainLoopObject, window):
 
     # Loop until the user closes the window
     while not glfw.window_should_close(window):
-        glClearColor(0.1, 0.1, 0.1, 1)
+        glClearColor(0.5, 0.5, 0.5, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         glUseProgram(shaderProgram)
+        glEnableVertexAttribArray(0)
 
         glBindVertexArray(VAO)
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, i)
-        # draw triangle using index
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, ctypes.c_void_p(0))
+        glBindBuffer(GL_ARRAY_BUFFER, pos)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index)
 
-        glBindVertexArray(0)
-        glUseProgram(0)
+
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, ctypes.c_void_p(0))
 
         # Swap front and back buffers
         glfw.swap_buffers(window)
 
+
         # Poll for and process events
         glfw.poll_events()
+
+        glBindVertexArray(0)
+        glUseProgram(0)
+    glfw.terminate()
 
 if __name__ == '__main__':
     main()
--- shaders/basic.frag
+++ shaders/basic.frag
@@ -1,9 +1,8 @@
 #version 330
 
-smooth in vec4 theColor;
 out vec4 outputColor;
 
 void main()
 {
-    outputColor = theColor;
+    outputColor = vec4(0.0,0.0,0.0,1.0);
 }
